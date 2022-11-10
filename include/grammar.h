0ac18af01301d354ab84a0fac1788c178680a0d6
Samuelmm15 <alu0101359526@ull.edu.es>
2022-11-10 13:26:02 +0000

    Added - the grammar class

---
+++ include/grammar.h
@@ -0,0 +1,23 @@
+/**
+ * @file grammar.h
+ * @author Samuel Martín Morales (alu0101359526@ull.edu.es)
+ * @brief 
+ * @version 0.1
+ * @date 2022-11-10
+ * @signature Computabilidad y Algoritmia.
+ * @course 2022-2023.
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+
+#pragma once
+
+class Grammar {
+
+};
